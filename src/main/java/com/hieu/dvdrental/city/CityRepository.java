b89d7a049113f2a6cfd8fa71d48af6553823e5b5
Hieu <hoanghieu010300@gmail.com>
2025-10-15 19:39:21 +0700

    City package initial commit

--- src/main/java/com/hieu/dvdrental/city/CityRepository.java
+++ src/main/java/com/hieu/dvdrental/city/CityRepository.java
@@ -1,8 +1,11 @@
 package com.hieu.dvdrental.city;
 
-import com.hieu.dvdrental.entity.City;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CityRepository extends JpaRepository<City, Integer> {
     boolean existsByCountryId(Integer id);
+    Page<City> findByNameContainingIgnoreCase(String name, Pageable pageable);
+    Page<City> findByCountryId(Integer id, Pageable pageable);
 }
